4b75cc3583e15f77d783d773a4d72860a1d8fefa
Georg Ostertag <georg@ostertage.de>
2026-03-18 20:01:11 +0100

    data retrieval

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+export basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export bindir="${basedir}/scripts"
+export datadir="${basedir}/data"
+export penvdir="${basedir}/penv"
+chmod 750 ${bindir}
+export PATH=${PATH}:${bindir}
+
+test -d $datadir || mkdir $datadir
+
+# setup virtual python
+if [ ! -d $penvdir ]; then
+  python3 -m venv $penvdir
+  pip install -r "${basedir}/requirements.txt"
+fi
+. "${penvdir}/bin/activate"
